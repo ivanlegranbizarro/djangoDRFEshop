ece3bfe86b902d1a7bef421465e7ec97170a0dd9
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-25 16:33:04 +0200

    get all products

--- product/admin.py
+++ product/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug", "created_at", "updated_at")
+    list_display = (
+        "name",
+        "slug",
+        "price",
+        "brand",
+        "category",
+        "created_at",
+    )
+    list_filter = ("category", "brand", "created_at")
--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -17,6 +18,7 @@ class Category(models.TextChoices):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
     description = models.TextField(max_length=500, validators=[MinLengthValidator(10)])
     price = models.DecimalField(max_digits=6, decimal_places=2)
     brand = models.CharField(max_length=50, validators=[MinLengthValidator(3)])
@@ -28,3 +30,13 @@ class Product(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
