796b26ef2559a09655ca6c35f43dc178f60a0d21
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-27 11:13:15 +0200

    protecting creation product route

--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .filters import ProductsFilter
@@ -42,13 +43,14 @@ def get_product(request, pk):
 
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def new_product(request):
     data = request.data
 
     serializer = ProductSerializer(data=data)
 
     if serializer.is_valid():
-        product = Product.objects.create(**data)
+        product = Product.objects.create(**data, user=request.user)
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
